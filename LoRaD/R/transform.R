3a6c760fd399d1c55ea26235333137bc423e6533
amilkey1 <analisa.milkey@gmail.com>
2023-02-04 15:22:13 -0500

    adding transform.R file

---
+++ LoRaD/R/transform.R
@@ -0,0 +1,7 @@
+# this function will log-transform "positive" parameters
+
+# load data
+param_file <- read.table("../test/params.log")
+
+# load conf file
+conf_file <- read.table("../test/loradml.conf")
